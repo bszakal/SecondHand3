c17a67e017de6508074741e4a543841dc2e3d40f
bszakal <bszakal22@fed.idserve.net>
2022-10-29 18:09:04 +0200

    Initial Commit

---
+++ SecondHand3/SecondHand3App.swift
@@ -0,0 +1,17 @@
+//
+//  SecondHand3App.swift
+//  SecondHand3
+//
+//  Created by Benjamin Szakal on 29/10/22.
+//
+
+import SwiftUI
+
+@main
+struct SecondHand3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
